951d1eb40c1e23345109b5624af798ff20973780
Jameszaa333 <cheer_james70@outlook.com>
2017-11-28 15:42:54 +0700

    yay
    This reverts commit 9e27217302bc7233881f7e3579aa8bb769cfcddb.

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,65 +1,30 @@
-const Thumbnails = require('../models/thumbnails')
-const News = require('../models/news')
-
 exports.index = function(req,res){
-
     let content = {
         highlight: {
             url: '/assets/images/home/banner/highlight.jpg',
             title: '7 For 7'
-        }
-//        ,
-//        thumbnail: [
-//            {
-//                url: '/assets/images/home/banner/thumbnail-1.jpg',
-//                title: 'Never Ever'
-//            },
-//            {
-//                url: '/assets/images/home/banner/thumbnail-2.jpg',
-//                title: 'Hard Carry'
-//            },
-//            {
-//                url: '/assets/images/home/banner/thumbnail-3.jpg',
-//                title: 'You Are'
-//            },
-//            {
-//                url: '/assets/images/home/banner/thumbnail-4.jpg',
-//                title: 'Fly'
-//            }
-//        ]
+        },
+        thumbnail: [
+            {
+                url: '/assets/images/home/banner/thumbnail-1.jpg',
+                title: 'Never Ever'
+            },
+            {
+                url: '/assets/images/home/banner/thumbnail-2.jpg',
+                title: 'Hard Carry'
+            },
+            {
+                url: '/assets/images/home/banner/thumbnail-3.jpg',
+                title: 'You Are'
+            },
+            {
+                url: '/assets/images/home/banner/thumbnail-4.jpg',
+                title: 'Fly'
+            }
+        ]
     }
-        const thumbnailsPromise = new Promise(function(resolve, reject){
-        Thumbnails.findAll().then(function(thumbnails){
-        resolve(thumbnails)
-    })
-})
-        const newsPromise = new Promise(function(resolve, reject){
-        News.findAll().then(function(news){
-        resolve(news)
-    })
-})
     
-    Promise.all([
-        thumbnailsPromise,
-        newsPromise
-    ]).then(function(values){
-        let thumbnails = values[0]
-        let news = values[1]
-        
-        content.thumbnails = thumbnails
-        content.news = news
-        
-        res.render('home.twig', content)
-    })
-    
-//    News.findAll().then(function(news){
-//        content.news = news
-//        res.render('home.twig', content)
-//    })
-    
-
-    res.render('home.twig')
-
+    res.render('home.twig', content)
 //    let content = {
 //        banner: [
 //            {
